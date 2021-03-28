e5622e5ab12c771fa6fd3eb0c03a4490c985a988
Sebastian Flores <sebastiandres@Sebastians-MacBook-Pro.local>
2021-03-28 14:40:03 -0300

    Aaaaalmost

--- .github/script.sh
+++ .github/script.sh
@@ -1,13 +1,5 @@
-# Get the filepath for this script 
-base="`dirname \"$0\"`"
-#echo $base
-
-# Get the relative path for the file to change
-subdir="/../../date/ever_changing_date_file.txt"
-#echo $subdir
-
-# Compose the right path for the date file
-new_path="$base$subdir"
+# Path to date file
+new_path="./date/ever_changing_date_file.txt"
 echo $new_path
 
 # Get the current date and time
@@ -17,3 +9,6 @@ new_date=$(date +%Y-%m-%d,%H:%M:%S)
 # Update the file
 echo $new_date > $new_path
 echo "Updated date for $GITHUB_REPOSITORY on $new_path to $new_date"
+
+# Check answer
+cat $new_path
